73aa8564ed3e4fcf0be887466ede74c4da8c875a
Ro <u202213765@upc.edu.pe>
2024-09-30 22:11:08 -0500

    feat: added base endpoint.

--- src/app/knowledge/services/knowledge.service.service.ts
+++ src/app/knowledge/services/knowledge.service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BaseService} from "../../shared/services/base.service.service";
+import {Knowledge} from "../models/knowledge.entity";
 
 @Injectable({
   providedIn: 'root'
 })
-export class KnowledgeServiceService {
+export class KnowledgeServiceService extends BaseService<Knowledge>{
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = "/knowledge";
+  }
 }
