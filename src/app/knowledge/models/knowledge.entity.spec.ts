5d75e1f1a1ed3de5607360634df4ef0637ae4671
Ro <u202213765@upc.edu.pe>
2024-09-30 20:03:18 -0500

    feat: added knowledge entity.

---
+++ src/app/knowledge/models/knowledge.entity.spec.ts
@@ -0,0 +1,7 @@
+import { KnowledgeEntity } from './knowledge.entity';
+
+describe('KnowledgeEntity', () => {
+  it('should create an instance', () => {
+    expect(new KnowledgeEntity()).toBeTruthy();
+  });
+});
---
+++ src/app/knowledge/models/knowledge.entity.ts
@@ -0,0 +1,13 @@
+export class Knowledge {
+  id: number;
+  title: string;
+  description: string;
+
+  constructor() {
+    this.id = 0;
+    this.title = '';
+    this.description = '';
+  }
+
+  
+}
